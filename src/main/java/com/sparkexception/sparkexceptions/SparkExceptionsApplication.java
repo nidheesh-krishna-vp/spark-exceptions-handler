4679d87b5186cd60bdc6cdfb299b6113c51ea964
nidheesh-krishna-vp <78521492+nidheesh-krishna-vp@users.noreply.github.com>
2021-02-23 13:23:38 +0530

    Add all my files

---
+++ src/main/java/com/sparkexception/sparkexceptions/SparkExceptionsApplication.java
@@ -0,0 +1,13 @@
+package com.sparkexception.sparkexceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkExceptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkExceptionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparkexception/sparkexceptions/SparkExceptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparkexception.sparkexceptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkExceptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
