1ae6a9e561f7d947bd0edb36b34c3424b82994df
nidheesh-krishna-vp <78521492+nidheesh-krishna-vp@users.noreply.github.com>
2021-02-25 10:59:46 +0530

    added changes

--- src/main/java/com/sparkexception/controller/TestController.java
+++ src/main/java/com/sparkexception/controller/TestController.java
@@ -2,15 +2,17 @@ package com.sparkexception.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 	Logger logger = LoggerFactory.getLogger(TestController.class);
 
-	@RequestMapping("/sample-url")
-	public void testMethod() {
+	@GetMapping(value = "/sample-url")
+	public ResponseEntity testMethod() {
 		logger.info("inside test method");
+		return null; 
 	}
 }
